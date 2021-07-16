989a755831ce344ccfac2311c8fad6be4f106603
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-16 00:52:55 -0500

    creando boton para conectarse a la base de datos y insertar valores personalizados a la tabla TProducts

--- ConexionSQL-WinUI3/MainWindow.xaml.cs
+++ ConexionSQL-WinUI3/MainWindow.xaml.cs
@@ -37,11 +37,7 @@ namespace ConexionSQL_WinUI3
                             txtContent.Text = Convert.ToString(dr["ProductURL"]);
                     }
                     */
-                    string insertArticle = "insert into TProducts(ProductName,ProductURL,UnitPriceTag,UnitPriceDesc,UnitPriceDescPorc,ProductTimePrice,ShippingPrice,StoreName,UnitsInStock)" +
-                                                        "values ('Tarjeta de video','https://articulo.mercadolibre.com.mx/MLM-753142586-base-de-maquillaje-piel-mixtagrasa-fit-me-matte-maybelline-_JM#reco_item_pos=1&reco_backend=promotions-sorted-by-score-mlm-A&reco_backend_type=low_level&reco_client=seller-promotions&reco_id=70e0364d-7d0c-418d-af31-36fef1eca084&DEAL_ID=MLM8566&S=landingHubrebajas-verano&V=20&T=CarouselDynamic-home&L=LAS-MEJORES-OFERTAS-EN-REBAJAS&deal_print_id=0fd024e0-e39a-11eb-8100-7bb62ad573bc&c_id=carouseldynamic-home&c_element_order=undefined&c_campaign=LAS-MEJORES-OFERTAS-EN-REBAJAS&c_uid=0fd024e0-e39a-11eb-8100-7bb62ad573bc',100,100,100,'2021-07-12',123,NULL,1)";
-                    SqlCommand comando = new SqlCommand(insertArticle, connection);
-                    comando.ExecuteNonQuery();
-                    connection.Close();
+                    //connection.Close();
                 }
             }
             catch (SqlException ex)
@@ -54,6 +50,24 @@ namespace ConexionSQL_WinUI3
             connection.Close();
             txtStatus.Text = connection.State.ToString();
         }
-
+        public void Insertar(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(TxtName.Text != "" && TxtUrl.Text != "")
+                {
+                    string insertArticle = "insert into TProducts(ProductName,ProductURL,UnitPriceTag,UnitPriceDesc,UnitPriceDescPorc,ProductTimePrice,ShippingPrice,StoreName,UnitsInStock)" +
+                                    "values ('" + TxtName.Text + "','" + TxtUrl.Text + "',100,100,100,'2021-07-12',123,NULL,1)";
+                    SqlCommand comando = new SqlCommand(insertArticle, connection);
+                    comando.ExecuteNonQuery();
+                    //connection.Close();
+                    txtStatus.Text = "Ok";
+                }
+            }
+            catch (SqlException ex)
+            {
+                txtStatus.Text = ex.ToString();
+            }
+        }
     }
 }
